eb0f5b033568ee7d8483f91e68d24979e0605bb2
BakedChicken77 <130414989+BakedChicken77@users.noreply.github.com>
2025-06-30 15:42:57 -0500

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e  # Exit on first error
+
+chmod +x codex_environment_setup_scripts/install_pgvector.sh
+./codex_environment_setup_scripts/install_pgvector.sh
+
+cd drsearch_backend
+cp .example.env .env
+poetry lock
+poetry install --all-extras
+
+cd ../drsearch_frontend
+cp .example.env .env
+yarn install
